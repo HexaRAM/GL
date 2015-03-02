4faba218d93a19c9ac50886926e9b418889ba231
Quentin Lemaire <quentyn.lemaire@gmail.com>
2015-03-02 10:08:57 +0100

    add regex test to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <boost/regex.hpp>
 using namespace std;
 
 int main ()
 {
 	cout << "Coucou l'HexaRAM+" << endl;
+    string pattern = "^\\d$";
+    string text = "43";
+    boost::regex re(pattern);
+
+    bool test = boost::regex_match(text, re);
+
+    if (test)
+    {
+        cout << "ça match !" << endl;
+    }
+    else 
+    {
+        cout << "ça ne match pas !" << endl;
+    }
+
 }
