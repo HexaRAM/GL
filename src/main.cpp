6842f089d263dbcbee72c088d3673374da52acea
Quentin Lemaire <quentyn.lemaire@gmail.com>
2015-03-14 21:04:57 +0100

    comment main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include "config.h"
 #include "commandLineReader.h"
 #include "automate/automate.h"
 using namespace std;
@@ -12,6 +13,11 @@ using namespace std;
 
 int main(int argc, char ** argv)
 {   
+
+    #ifdef DEBUG
+        cout << "# Le mode DEBUG est activé !" << endl << endl;
+    #endif
+
     CommandLineReader console(argc, argv);
     console.displayMode();
     Automate* automate = console.createAutomate();
@@ -22,68 +28,70 @@ int main(int argc, char ** argv)
         exit(1);
     }
 
-    automate->addVariable("a");
-    automate->addVariable("b");
-    automate->addConstante("c", 69);
-    automate->instanciateVariable("a", 100);
-    automate->instanciateVariable("e", 200);
-    automate->addVariable("c");
-    automate->addConstante("b", 78);
+    automate->execute();
 
-    cout << "\tEtat de la mémoire : " << endl;
-    automate->displayMemory();
+    // automate->addVariable("a");
+    // automate->addVariable("b");
+    // automate->addConstante("c", 69);
+    // automate->instanciateVariable("a", 100);
+    // automate->instanciateVariable("e", 200);
+    // automate->addVariable("c");
+    // automate->addConstante("b", 78);
 
-    // test Lexer
+    // cout << "\tEtat de la mémoire : " << endl;
+    // automate->displayMemory();
+
+    // // test Lexer
 
 
-    string press;
-    while (true)
-    {
-        cout << endl;
-        cout << "--------------------" << endl;
-        cout << "------- MENU -------" << endl;
-        cout << "--------------------" << endl;
-        cout << "a : affichage code" << endl;
-        cout << "b : display buffer" << endl;
-        cout << "s : stop program" << endl;
-        cout << "n : display all next " << endl;
-        cout << "--------------------" << endl;
-        cout << " > ";
-        getline(cin, press);
-        cout << endl;
+    // string press;
+    // while (true)
+    // {
+    //     cout << endl;
+    //     cout << "--------------------" << endl;
+    //     cout << "------- MENU -------" << endl;
+    //     cout << "--------------------" << endl;
+    //     cout << "a : affichage code" << endl;
+    //     cout << "b : display buffer" << endl;
+    //     cout << "s : stop program" << endl;
+    //     cout << "n : display all next " << endl;
+    //     cout << "--------------------" << endl;
+    //     cout << " > ";
+    //     getline(cin, press);
+    //     cout << endl;
 
-        if (press == "s")
-        {
-            break;
-        }
-        else if (press == "a")
-        {
-            automate->execute(AFFICHAGE);
-        }
-        else if (press == "b")
-        {
-            automate->displayBuffer();
-        }
-        else if (press == "n")
-        {
-            string next = automate->getNext();
-            while (next != "$" && next != "Erreur - aucun pattern trouvé")
-            {
-                cout << "# Prochain symbole : " << next << endl;
-                next = automate->getNext();
+    //     if (press == "s")
+    //     {
+    //         break;
+    //     }
+    //     else if (press == "a")
+    //     {
+    //         automate->execute(AFFICHAGE);
+    //     }
+    //     else if (press == "b")
+    //     {
+    //         automate->displayBuffer();
+    //     }
+    //     else if (press == "n")
+    //     {
+    //         string next = automate->getNext();
+    //         while (next != "$" && next != "Erreur - aucun pattern trouvé")
+    //         {
+    //             cout << "# Prochain symbole : " << next << endl;
+    //             next = automate->getNext();
 
-                if (next == "$")
-                {
-                    cout << "# Prochain symbole : " << next << endl << "\tFin de la lecture" << endl;
-                }
-            }
-        }
-        else
-        {
-            string next = automate->getNext();
-            cout << "# Prochain symbole : " << next << endl;
-        }
-    }
+    //             if (next == "$")
+    //             {
+    //                 cout << "# Prochain symbole : " << next << endl << "\tFin de la lecture" << endl;
+    //             }
+    //         }
+    //     }
+    //     else
+    //     {
+    //         string next = automate->getNext();
+    //         cout << "# Prochain symbole : " << next << endl;
+    //     }
+    // }
 
     delete automate;
 }
