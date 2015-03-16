3a31077f9b55eadd2f47c7d9b0ac462e16c70fba
Thabet Zied <zthabet@if501-219-17.insa-lyon.fr>
2015-03-16 08:53:11 +0100

    uncomment state29

--- src/etat/etat29.cpp
+++ src/etat/etat29.cpp
@@ -1,9 +1,9 @@
 #include "etat29.h"
-//#include "etat22.h"
-//#include "etat23.h"
-//#include "etat24.h"
+#include "etat22.h"
+#include "etat23.h"
+#include "etat24.h"
 #include "etat39.h"
-//#include "etat21.h"
+#include "etat21.h"
 #include "../config.h"
 
 
@@ -15,19 +15,19 @@ bool Etat29::transition(Automate & automate, Symbole * s ){
 	int idSym = *s ; 
 	switch (idSym) {
 		case id :
-			//automate.decalage(s, new Etat22);
+			automate.decalage(s, new Etat22);
 			break;
 		case num :
-			//automate.decalage(s, new Etat23):
+			automate.decalage(s, new Etat23):
 			break;
 		case po :
-			//automate.decalage(s, new Etat24);
+			automate.decalage(s, new Etat24);
 			break;
 		case T : 
 			automate.decalage(s, new Etat39);
 			break;
 		case F :
-			//automate.decalage(s, new Etat21);
+			automate.decalage(s, new Etat21);
 		default : break;
 	}
 	return false;
