e99d8c7493ec4be531bc3b4ec7578a7e6a000ce0
MariaETEGAN <maria.etegan@gmail.com>
2015-03-28 11:54:44 +0100

    execution v2

--- src/automate/execution.cpp
+++ src/automate/execution.cpp
@@ -19,10 +19,6 @@ bool Execution::execute(Memory& mem)
     // création d'une map associant les identifiants à leur valeur directement pour l'évaluation des expressions
     map<string, double> map_val = mem.buildEvalMap();
 
-    //cout<<variables.size()<<endl;
-    //cout<<constantes.size()<<endl;
-    //cout<<map_val.size()<<endl;
-
     for (auto const &it : liste_instruction)
     {
         Affectation *aff = dynamic_cast<Affectation*> (it);
@@ -31,37 +27,41 @@ bool Execution::execute(Memory& mem)
 
         if(aff != NULL)
         {
-            // c'est une affectation
-            cout << "----------------------" << endl;
-            //TODO addVariable()
+            //Affectation
 
             double valExpr = (aff->getExpression())->eval(map_val);
 
-            map_val[*(aff->getIdentificateur())] = valExpr;
-            cout << *aff << "AFF: La variable '" << *(aff->getIdentificateur()) <<"' a la valeur " << valExpr << endl;
-            cout << "La taille de ma map: " << map_val.size() << endl;
+            if(mem.instanciateVariable(*(aff->getIdentificateur()), (int)valExpr)) {
+
+                map_val[*(aff->getIdentificateur())] = valExpr;
+
+            }else{
+                return false;
+            }
+                        
 
         }
         else if(lec != NULL)
         {
-            cout << "----------------------"  << endl;
-            cout << *lec << "LEC: Je lis une valeur pour la variable '" <<  *(lec->getIdentificateur()) << "'" << endl;
-            
-            double val = 0; 
-            cin >> val;
+            //Lecture
 
-            cout << "J'ai lu " << *(lec->getIdentificateur()) << " = " <<val << endl;
-
-            //Je rajoute dans ma map la nouvelle valeur de la var
-            map_val[*(lec->getIdentificateur())] = val;
-            cout << "La taille de ma map: " << map_val.size() << endl;
-            //TODO addVariable()
+            int val; 
+            if (cin >> val) {
+                   
+                if(mem.instanciateVariable(*(lec->getIdentificateur()), val)) {
+                    map_val[*(lec->getIdentificateur())] = val;            
+                }else{
+                return false;
+                }
+            }else{
+                return false;
+            }      
             
         }
         else if(ecr != NULL)
         {
-            cout << "----------------------" << endl;
-            cout << *ecr << "J'ecris: " << ecr->getExpression()->eval(map_val) << endl;
+            //Ecriture
+            cout << ecr->getExpression()->eval(map_val) << endl;
        }
     }
     
